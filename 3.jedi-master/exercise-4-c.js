b15d8b22ac12315a580d6fca1e69fdea23806cb9
JuFML <julianafernandezlima@hotmail.com>
2024-01-15 12:10:54 -0300

    Exercise#4-c

---
+++ 3.jedi-master/exercise-4-c.js
@@ -0,0 +1,77 @@
+//*!-------------------------------------------------------------------------------------------------------------------------------------
+//*? Basandote en el ejercicio anterior. Crea un botón para cada uno de los elementos de las listas que elimine ese mismo elemento del html.
+//*!-------------------------------------------------------------------------------------------------------------------------------------
+
+const places = [
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570610155223-66279ba81b41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570888233388-35d777042d9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1490440101319-4c8eb3880432?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1460453429228-912eec8be349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+];
+
+function createList(infos) {
+  let itemListComponent = ""
+
+  infos.forEach(({ title, imgUrl }, index) => {
+    itemListComponent +=
+      `<div id=${index + 1} class="list-item">
+        <h4>${title}</h4>
+        <img src=${imgUrl} alt=${title}>
+        <button>Remove this element</button>
+      </div>
+      `
+  });
+
+  renderList(itemListComponent)
+}
+
+function renderList(list) {
+  document.querySelector("body").innerHTML += list
+}
+
+function addButtonClickEvent() {
+  document.querySelectorAll("button").forEach((btn, index) => {
+    if (btn.textContent === "Remove last element") {
+      btn.addEventListener("click", () => removeLastListItem())
+      return
+    }
+    removeCurrentItem(btn, index)
+  })
+}
+
+function removeLastListItem() {
+  document.querySelectorAll(".list-item").forEach((item, index, array) => {
+    if (index == array.length - 1) {
+      item.remove()
+    }
+  })
+}
+
+function removeCurrentItem(btn, id) {
+  const itemToBeRemoved = document.getElementById(id)
+  btn.addEventListener("click", () => itemToBeRemoved.remove())
+}
+
+createList(places)
+addButtonClickEvent()
