17e4ca2d370703db2490a7bd9548f4f381489cdd
JuFML <julianafernandezlima@hotmail.com>
2024-01-15 18:53:51 -0300

    Extra Exercise#5

---
+++ extra/exercise-5.js
@@ -0,0 +1,14 @@
+//*!-------------------------------------------------------------------------------------------------------------------------------------
+//*? Al siguiente javascript no funciona como deberia...podrias arreglarlo?
+//*? Debería mostrar por consola "Pepito Palotes".
+//*!-------------------------------------------------------------------------------------------------------------------------------------
+function a() {
+  const name = "Pepito Palotes";
+  b(name);
+}
+
+function b(name) {
+  console.log(name);
+}
+
+a();
